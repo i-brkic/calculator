08f4a97aad880c3b584815449decaa335a6e3000
i-brkic <184224468+i-brkic@users.noreply.github.com>
2025-01-17 13:51:53 +0100

    Write a function that executes an operation
    Write a function which will take two numbers and
    a function and which will then execute mathematical
    operation based on a given function and return the
    result.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// 1. functions for basic calculator operations
+// 1. Functions for basic calculator operations
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+// 2. Variables to hold number, operator and the other number, will use them to update display later
+
+let number1;
+let operator;
+let number2;
+
+// 3. Function which takes the operator and two numbers and then calls one of the functions above
+
+const operate = (a, operation, b) => operation(a, b);
