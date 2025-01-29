ea2a7fbfc8d451415fa4e627dbd9879f02f34363
i-brkic <184224468+i-brkic@users.noreply.github.com>
2025-01-29 15:11:41 +0100

    Write a function to populate the calculator display
    Written a function which populates the calculator display and
    updates a number1 variable so that it can be used in an operation.

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ let number2;
 // 3. Function which takes the operator and two numbers and then calls one of the functions above
 
 const operate = (a, operation, b) => operation(a, b);
+
+// 4. Write a function to populate the display when you click the digit buttons.
+
+const numbers = document.querySelectorAll(".number");
+const display = document.querySelector(".display");
+
+numbers.forEach(element => {
+  element.addEventListener("click", () => {
+    number1 =
+      display.textContent === "0"
+        ? element.textContent
+        : (number1 += element.textContent);
+    display.textContent = number1;
+  });
+});
