b0f394de0412fd4b761975e515c17f1676f913e9
i-brkic <184224468+i-brkic@users.noreply.github.com>
2025-02-01 16:46:08 +0100

    Rework functionality for one operation at the time
    Reworked functionality of operations event listener, now being
    able calculate one operation at the time. If operation button is
    pressed again when there are 2 numbers and an operator already
    defined, operation will execute and save the new operator while
    reseting number 2.

--- script.js
+++ script.js
@@ -48,45 +48,81 @@ const allClear = document.querySelector(".clear");
 
 operations.forEach(element => {
   element.addEventListener("click", e => {
-    if (display.textContent === "0") {
+    if (number1 === undefined) {
       alert("Please enter a number!");
+      return;
     }
+
+    // Execute one operation at a time, if an operation is pressed again, execute it and populate the display, while saving the next operator and preparing number 2 to be inputed again.
+    if (operator && number2 !== undefined) {
+      const num1 = parseFloat(number1);
+      const num2 = parseFloat(number2);
+      let result;
+
+      switch (operator) {
+        case "/":
+          if (num2 !== 0) {
+            result = operate(num1, divide, num2);
+          } else {
+            alert("Can't divide by zero! Enter valid number.");
+          }
+          break;
+        case "*":
+          result = operate(num1, multiply, num2);
+          break;
+        case "-":
+          result = operate(num1, subtract, num2);
+          break;
+        case "+":
+          result = operate(num1, add, num2);
+          break;
+      }
+
+      display.textContent = result;
+      number1 = result.toString();
+      number2 = undefined;
+    }
+
     operator = e.target.innerText;
   });
-});
-
-allClear.addEventListener("click", () => {
-  number1 = undefined;
-  number2 = undefined;
-  operator = undefined;
-  display.textContent = 0;
 });
 
 equal.addEventListener("click", () => {
   if (number1 !== undefined && number2 !== undefined && operator) {
     const num1 = parseFloat(number1);
     const num2 = parseFloat(number2);
+    let result;
 
     switch (operator) {
       case "/":
         if (number2 !== 0) {
-          display.textContent = operate(num1, divide, num2);
+          result = operate(num1, divide, num2);
         } else {
-          alert("Cant divide by zero! AYOOOOOO");
+          alert("Cant divide by zero!");
         }
         break;
       case "*":
-        display.textContent = operate(num1, multiply, num2);
+        result = operate(num1, multiply, num2);
         break;
       case "-":
-        display.textContent = operate(num1, subtract, num2);
+        result = operate(num1, subtract, num2);
         break;
       case "+":
-        display.textContent = operate(num1, add, num2);
+        result = operate(num1, add, num2);
         break;
     }
-    number1 = display.textContent;
+    display.textContent = result;
+    number1 = result.toString();
     number2 = undefined;
     operator = undefined;
   }
+});
+
+// 6. Reset button to start everything from the beginning.
+
+allClear.addEventListener("click", () => {
+  number1 = undefined;
+  number2 = undefined;
+  operator = undefined;
+  display.textContent = "0";
 });
